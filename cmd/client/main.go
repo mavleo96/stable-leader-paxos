1e7fba069cadb0a06c436cec488478c4fc7ce683
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-02 19:46:49 -0400

    refactor clients to process transactions from shared memory instead of channels

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,74 +4,154 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
-	models "github.com/mavleo96/cft-mavleo96/internal/models"
+	log "github.com/sirupsen/logrus"
 )
 
-var clients [10]string = [10]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+var clients []string = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
 
-func main() {
-	fmt.Println("DEBUG: client application started...")
+// transaction represents the structure of transaction to be processed
+type transaction struct {
+	Sender   string
+	Receiver string
+	Amount   int
+}
 
-	fmt.Println("DEBUG: spawning clients...")
-	clientChannels := make(map[string]chan models.Transaction)
-	for _, client_id := range clients {
-		// TODO: review if a bounded channel buffer is the correct design choice?
-		clientChannels[client_id] = make(chan models.Transaction, 10)
-		go clientRoutine(client_id, clientChannels[client_id])
-		defer close(clientChannels[client_id])
+// parseTransaction parses a transaction string of the format "(Sender, Receiver, Amount)"
+func parseTransaction(s string) (transaction, error) {
+	var p []string = strings.Split(strings.Trim(s, "()\""), ", ")
+	amount, err := strconv.Atoi(p[2])
+	if err != nil {
+		return transaction{}, err
 	}
 
+	return transaction{p[0], p[1], amount}, nil
+}
+
+// parseNodeString parses a string representation of a list of nodes
+// func parseNodeString(s string) []string {
+// 	return strings.Split(strings.Trim(s, "[]\""), ", ")
+// }
+
+func main() {
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+
+	// Parse Transactions
+	// The entire csv file is loaded into memory and transactions are queued by
+	// client and set number like preserving the order for each client
 	file, err := os.Open("test1.csv")
 	if err != nil {
-		fmt.Println("error opening file:", err)
-		return
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
-		fmt.Println("error reading records:", err)
+		log.Fatal(err)
 	}
 
+	// txnQueue is a nested map of client id to set number to list of transactions
+	txnQueue := make(map[string]map[int][]transaction)
+	for _, clientID := range clients {
+		txnQueue[clientID] = make(map[int][]transaction)
+	}
+
+	// Process records
+	var setNum int
 	for i, record := range records {
 		if i == 0 {
-			continue
+			continue // skip header row
 		}
+
+		// If set number is new, initialize new lists for each client
 		if record[0] != "" {
-			fmt.Scanln()
-			fmt.Printf("DEBUG: processing set number %s...\n", record[0])
+			var err error
+			setNum, err = strconv.Atoi(record[0])
+			if err != nil {
+				fmt.Println("error parsing set number:", err)
+			}
+			for _, clientID := range clients {
+				txnQueue[clientID][setNum] = make([]transaction, 0)
+			}
 
-			nodeList := parseNodeString(record[2])
-			fmt.Printf("DEBUG: alive nodes %v...\n", nodeList)
-			// TODO: need to send signals to stop inter node communication?
+			// TODO: need to save alive nodes list here
 		}
-		transaction, err := models.ParseTransaction(record[1])
+
+		// Parse and append transaction
+		t, err := parseTransaction(record[1])
 		if err != nil {
-			fmt.Println("error parsing transaction:", err)
+			log.Fatal(err)
 		}
-		clientChannels[transaction.Sender] <- transaction
+		txnQueue[t.Sender][setNum] = append(txnQueue[t.Sender][setNum], t)
 	}
-	// TODO: review what to do when main has exhausted parsing records
-	// wait for channel goroutines to finish or close on prompt
-	fmt.Scanln()
-}
 
-func parseNodeString(s string) []string {
-	return strings.Split(strings.Trim(s, "[]\""), ", ")
-}
+	// Client Routines
+	// Each client has its own goroutine and channel
+	// The channel is used by main routine to inform which set to process next
+	// and client uses it to inform main when it is done processing the set
+	clientChannels := make(map[string]chan int)
+	for _, clientID := range clients {
+		clientChannels[clientID] = make(chan int)
+		defer close(clientChannels[clientID])
+		go clientRoutine(clientID, clientChannels[clientID], txnQueue)
+	}
 
-func clientRoutine(client_id string, channel <-chan models.Transaction) {
-	fmt.Printf("DEBUG: client %s running...\n", client_id)
-	for {
-		transaction := <-channel
-		fmt.Printf("DEBUG: client %s processing transaction %v at %d\n", client_id, transaction, time.Now().UnixNano())
-		if client_id != "A" {
-			// emulating processing for now
-			time.Sleep(time.Second)
+	// Main Interactive Loop to control set processing
+	for i := 1; i <= setNum; i++ {
+		// Signal all clients to process set i and wait for all to finish
+		for _, clientID := range clients {
+			clientChannels[clientID] <- i
 		}
+		for _, clientID := range clients {
+			<-clientChannels[clientID]
+		}
+		log.Info("Set ", i, " processed by all clients")
+
+		// Interaction loop
+		var cmd string
+	interactionLoop:
+		for {
+			_, err := fmt.Scan(&cmd)
+			if err != nil {
+				log.Panic(err)
+			}
+			switch cmd {
+			case "next":
+				break interactionLoop
+			case "exit":
+				log.Info("Exiting...")
+				return
+			case "kill leader":
+				fmt.Print(cmd)
+				// TODO: implement kill leader
+			case "print log":
+				// TODO: implement print log
+			case "print db":
+				// TODO: implement print db
+			case "print status":
+				// TODO: implement print status
+			case "print view":
+				// TODO: implement print view
+			default:
+				continue
+			}
+		}
+	}
+	// TODO: implement graceful shutdown
+}
+
+func clientRoutine(clientID string, channel chan int, txnQueue map[string]map[int][]transaction) {
+	for {
+		setNum := <-channel
+
+		for _, t := range txnQueue[clientID][setNum] {
+			log.Infof("Client %s processing transaction: %v", clientID, t)
+			time.Sleep(500 * time.Millisecond) // simulate processing time
+		}
+		channel <- 0
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/mavleo96/cft-mavleo96
 go 1.25.1
 
 require (
-	go.etcd.io/bbolt v1.4.3 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
+	go.etcd.io/bbolt v1.4.3
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )

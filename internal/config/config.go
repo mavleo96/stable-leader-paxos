41de5ba37004fef54155b946ba860a7e5aed6865
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-06 03:01:13 -0400

    store node pointer in config struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,9 +9,9 @@ import (
 
 // Config holds the configuration for the distributed system
 type Config struct {
-	Nodes   []models.Node `yaml:"nodes"`
-	Clients []string      `yaml:"clients"`
-	DBDir   string        `yaml:"db_dir"`
+	Nodes   []*models.Node `yaml:"nodes"`
+	Clients []string       `yaml:"clients"`
+	DBDir   string         `yaml:"db_dir"`
 }
 
 func ParseConfig(cfgPath string) (*Config, error) {

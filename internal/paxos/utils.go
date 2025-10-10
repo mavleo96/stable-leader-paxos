9c4cdba44384ed196d8faa6741a9cef7219aad79
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-10 01:31:29 -0400

    add utils functions for logging and paxos

--- internal/paxos/utils.go
+++ internal/paxos/utils.go
@@ -1,8 +1,14 @@
 package paxos
 
 import (
+	"crypto/rand"
+	"math/big"
+	"time"
+
 	pb "github.com/mavleo96/cft-mavleo96/pb/paxos"
 )
+
+// BallotNumber Utilities
 
 // BallotNumberIsHigherOrEqual checks if current >= new
 func BallotNumberIsHigherOrEqual(current *pb.BallotNumber, new *pb.BallotNumber) bool {
@@ -18,4 +24,35 @@ func BallotNumberIsHigher(current *pb.BallotNumber, new *pb.BallotNumber) bool {
 		return new.NodeID > current.NodeID
 	}
 	return new.N > current.N
+}
+
+// AcceptLog Utilities
+
+// GetSequenceNumberIfExistsInAcceptLog gets the sequence number from the accept log
+func GetSequenceNumberIfExistsInAcceptLog(acceptLog map[int64]*pb.AcceptRecord, req *pb.TransactionRequest) (int64, bool) {
+	for _, record := range acceptLog {
+		if record.AcceptedVal.Sender == req.Sender && record.AcceptedVal.Timestamp == req.Timestamp {
+			return record.AcceptedSequenceNumber, true
+		}
+	}
+	return 0, false
+}
+
+// MaxSequenceNumber gets the max sequence number from the accept log
+func MaxSequenceNumber(acceptLog map[int64]*pb.AcceptRecord) int64 {
+	max := int64(0)
+	for _, record := range acceptLog {
+		if record.AcceptedSequenceNumber > max {
+			max = record.AcceptedSequenceNumber
+		}
+	}
+	return max
+}
+
+// Timer Utilities
+
+// RandomTimeout returns a random timeout between min and max
+func RandomTimeout(min time.Duration, max time.Duration) (time.Duration, error) {
+	n, err := rand.Int(rand.Reader, big.NewInt(max.Milliseconds()-min.Milliseconds()))
+	return time.Duration(n.Int64()+min.Milliseconds()) * time.Millisecond, err
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,48 +6,48 @@ import (
 	pb "github.com/mavleo96/cft-mavleo96/pb/paxos"
 )
 
-func TransactionResponseString(resp *pb.TransactionResponse) string {
+func TransactionResponseString(x *pb.TransactionResponse) string {
 	return fmt.Sprintf(
 		"<REPLY, %s, %d, %s, %t>",
-		BallotNumberString(resp.B),
-		resp.Timestamp,
-		resp.Sender,
-		resp.Result,
+		BallotNumberString(x.B),
+		x.Timestamp,
+		x.Sender,
+		x.Result,
 	)
 }
 
-func AcceptedMessageString(record *pb.AcceptedMessage) string {
-	return fmt.Sprintf(
-		"<ACCEPTED, %s, %d, %s, %s>",
-		BallotNumberString(record.B),
-		record.SequenceNum,
-		TransactionRequestString(record.Message),
-		record.AcceptorID,
-	)
-}
+// func AcceptedMessageString(x *pb.AcceptedMessage) string {
+// 	return fmt.Sprintf(
+// 		"<ACCEPTED, %s, %d, %s, %s>",
+// 		BallotNumberString(x.B),
+// 		x.SequenceNum,
+// 		TransactionRequestString(x.Message),
+// 		x.AcceptorID,
+// 	)
+// }
 
-func AcceptRecordString(record *pb.AcceptRecord) string {
+func AcceptRecordString(x *pb.AcceptRecord) string {
 	return fmt.Sprintf(
 		"<ACCEPT, %s, %d, %s>",
-		BallotNumberString(record.AcceptedBallotNumber),
-		record.AcceptedSequenceNumber,
-		TransactionRequestString(record.AcceptedVal),
+		BallotNumberString(x.AcceptedBallotNumber),
+		x.AcceptedSequenceNumber,
+		TransactionRequestString(x.AcceptedVal),
 	)
 }
 
-func TransactionRequestString(req *pb.TransactionRequest) string {
+func TransactionRequestString(x *pb.TransactionRequest) string {
 	return fmt.Sprintf(
 		"<REQUEST, %s, %d, %s>",
-		TransactionString(req.Transaction),
-		req.Timestamp,
-		req.Sender,
+		TransactionString(x.Transaction),
+		x.Timestamp,
+		x.Sender,
 	)
 }
 
-func BallotNumberString(ballot *pb.BallotNumber) string {
-	return fmt.Sprintf("(%s, %d)", ballot.NodeID, ballot.N)
+func BallotNumberString(x *pb.BallotNumber) string {
+	return fmt.Sprintf("<%d, %s>", x.N, x.NodeID)
 }
 
-func TransactionString(req *pb.Transaction) string {
-	return fmt.Sprintf("(%s, %s, %d)", req.Sender, req.Receiver, req.Amount)
+func TransactionString(x *pb.Transaction) string {
+	return fmt.Sprintf("(%s, %s, %d)", x.Sender, x.Receiver, x.Amount)
 }
